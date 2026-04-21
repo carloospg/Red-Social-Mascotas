de59f9d1fbc93621fdd0f20bca7d778329ddb1af
carloospg <carlosperezgosalvez@gmail.com>
2026-04-21 18:59:08 +0200

    feat: añado el registro

---
+++ backend/src/users/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import  { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export enum UserRole {
+    ADMIN = 'admin',
+    USER = 'user',
+}
+
+@Schema({ collection: 'users', timestamps: true })
+export class User extends Document {
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ required: true, unique: true, index: true, lowercase: true})
+    email: string;
+
+    @Prop({ required: true })
+    password: string;
+
+    @Prop({ enum: UserRole, default: UserRole.USER})
+    rol: UserRole;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+UserSchema.set('versionKey', false)
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,2 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { renderRegister } from './pages/register';
+
+const app = document.getElementById('app')!;
+renderRegister(app);
