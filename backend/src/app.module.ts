6d1f9413166e32d490b95719bf47db6630fbaa0a
carloospg <carlosperezgosalvez@gmail.com>
2026-04-21 17:43:27 +0200

    feat: añado la configuracion inicial

---
+++ backend/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule} from '@nestjs/mongoose';
+
+// Lo hago asi en vez de la manera que hay en los apuntes porque asi lee la URI desde el .env usando ConfigService 
+// y asi el codigo no tiene ninguna credencial, de la otra forma estaba hardcodeada y quedaban expuestas las credenciales, 
+// no creo que sea muy importante ahora mismo para estos ejercicios, pero lo veo mejor practica
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService]
+    })
+  ],
+})
+export class AppModule {}
