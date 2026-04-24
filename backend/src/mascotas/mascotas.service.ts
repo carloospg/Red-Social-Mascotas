c548458e0bcdfa9989b28661aa627ce26c7e3169
carloospg <carlosperezgosalvez@gmail.com>
2026-04-24 13:56:07 +0200

    feat: añado el update y delete al service

--- backend/src/mascotas/mascotas.service.ts
+++ backend/src/mascotas/mascotas.service.ts
@@ -1,4 +1,5 @@
 import {
+  ForbiddenException,
   Injectable,
   NotFoundException,
 } from "@nestjs/common";
@@ -6,7 +7,8 @@ import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Mascota } from "./entities/mascota.entity";
 import { CreateMascotaDto } from "./dto/create-mascota.dto";
-import { User } from "src/users/entities/user.entity";
+import { User, UserRole } from "src/users/entities/user.entity";
+import { UpdateMascotaDto } from "./dto/update-mascota.dto";
 
 @Injectable()
 export class MascotasService {
@@ -17,12 +19,12 @@ export class MascotasService {
   async create(
     createMascotaDto: CreateMascotaDto,
     user: User,
-    urlFoto?: string
+    urlFoto?: string,
   ): Promise<Mascota> {
     return await this.mascotaModel.create({
       ...createMascotaDto,
       propietario: user._id,
-      urlFoto: urlFoto || '',
+      urlFoto: urlFoto || "",
     });
   }
 
@@ -48,5 +50,55 @@ export class MascotasService {
     return await this.mascotaModel
       .find({ propietario: userId })
       .populate("propietario", "nombre email");
+  }
+
+  async update(
+    id: string,
+    updateMascotaDto: UpdateMascotaDto,
+    user: User,
+    urlFoto?: string,
+  ): Promise<Mascota | null> {
+    const mascota = await this.mascotaModel.findById(id);
+
+    if (!mascota) {
+      throw new NotFoundException("Mascota no encontrada");
+    }
+
+    if (
+      mascota.propietario.toString() !== user._id.toString() &&
+      user.rol !== UserRole.ADMIN
+    ) {
+      throw new ForbiddenException(
+        "No tienes permiso para editar esta mascota",
+      );
+    }
+
+    const updateData: any = { ...updateMascotaDto };
+    if (urlFoto) {
+      updateData.urlFoto = urlFoto;
+    }
+
+    return await this.mascotaModel.findByIdAndUpdate(id, updateData, {
+      new: true,
+    });
+  }
+
+  async delete(id: string, user: User): Promise<void> {
+    const mascota = await this.mascotaModel.findById(id);
+
+    if (!mascota) {
+      throw new NotFoundException("Mascota no encontrada");
+    }
+
+    if (
+      mascota.propietario.toString() !== user._id.toString() &&
+      user.rol !== UserRole.ADMIN
+    ) {
+      throw new ForbiddenException(
+        "No tienes permiso para eliminar esta mascota",
+      );
+    }
+
+    await this.mascotaModel.findByIdAndDelete(id);
   }
 }
